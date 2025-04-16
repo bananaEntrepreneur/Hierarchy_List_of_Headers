8e7e07b68aece438ed530e2afcde723792111c3f
Artem <artem.kabatov22@gmail.com>
2025-04-16 13:53:52 +0300

    initial commit

---
+++ HierarchyListOfHeaderTags/functions.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "main.h"
+
+/*! Определить числовой уровень заголовка (H1=1, H2=2, ...)
+* \param [in] element - ссылка на элемент DOM дерева
+* return числовой уровень заголовка. 0 если элемент не является валидным тегом заголовка H1-H6
+*/
+int getHeaderLevel(const QDomElement& element);
+
+/*! Найти родителя в иерархическом списке
+* \param [in] previous - указатель на предыдущий пункт в списке 
+* \param [in] currentLevel - уровень текущего заголовочного тега
+* return указатель на родителя
+*/
+Paragraph* findParentForParagraph(Paragraph* previous, int currentLevel);
+
+
+/*! Рекурсивная функция построения иерархии заголовочных тегов
+* \param [in] domTreeRoot - корень DOM дерева  
+* \param [in,out] root - корень иерархии заголовочных тегов
+* \param [in] errors - контейнер ошибок
+*/
+void createHierarchyListOfHeaderTags(QDomElement* domTreeRoot, Paragraph* root, QSet<Error>& errors);
---
+++ HierarchyListOfHeaderTags/main.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <QtCore/QCoreApplication>
+#include <qxml>
+#include <qdom>
+#include <qset>
+#include "Paragraph.h"
+#include "Error.h"
+#include "functions.h"
