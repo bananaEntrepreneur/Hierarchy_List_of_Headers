8ec9c900e53ac60ef8a0471dc47720b2c9fa660c
Artem <artem.kabatov22@gmail.com>
2025-05-26 23:55:25 +0300

    minor changes

--- main.cpp
+++ main.cpp
@@ -4,10 +4,16 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    QSet<Error> errors;
 
     QFile inputFile(argv[1]);
 
-    inputFile.open(QIODevice::ReadOnly);
+    if (!inputFile.open(QIODevice::ReadOnly)) {
+        Error fileError;
+        fileError.setType(ErrorType::fileError);
+        fileError.setErrorInputPath(argv[1]);
+        errors.insert(fileError);
+    }
 
     QDomDocument domTree;
     QString errorMesaage;
@@ -21,14 +27,12 @@ int main(int argc, char *argv[])
 
     Paragraph root;
 
-    QSet<Error> errors;
-
     createHierarchyListOfHeaderTags(bodyElement, &root, errors);
 
     QFile outputFile(argv[2]);
 
     if (!outputFile.open(QIODevice::WriteOnly | QIODevice::Text))
-        return false;
+        return 1;
 
     return a.exec();
 }
