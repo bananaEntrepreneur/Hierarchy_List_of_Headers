ffa71d253bbe98bbc8df2f66e6bc070d433e3cc8
Artem <artem.kabatov22@gmail.com>
2025-06-04 17:01:11 +0300

    bat-tests added

---
+++ bat-tests/tester.bat
@@ -0,0 +1,3 @@
+Hierarchy_List_of_Headers.exe < D:\Hierarchy_List_of_Headers\bat-tests\Hierarchy_List_of_Headers.exe < D:\Hierarchy_List_of_Headers\bat-tests\01_basic.html < D:\Hierarchy_List_of_Headers\bat-tests\results.txt
+
+Hierarchy_List_of_Headers.exe < D:\Hierarchy_List_of_Headers\bat-tests\Hierarchy_List_of_Headers.exe < D:\Hierarchy_List_of_Headers\bat-tests\02_complex.html < D:\Hierarchy_List_of_Headers\bat-tests\results.txt
